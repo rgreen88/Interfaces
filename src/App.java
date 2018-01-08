df2e2f040ae6de2db61970371b403b0ad866c9f6
rgreen88 <rynelgreen@gmail.com>
2018-01-07 23:34:20 -0500

    Finished Interface practice. Additional comments explaining pointers and references

--- src/App.java
+++ src/App.java
@@ -11,6 +11,25 @@ public class App {
 		person1.greet();
 		
 		//creating showInfo() method, but both classes are not the same, so we make use of interfaces
+		//creating new class objects allocate memory to store variables
+		
+		Info info1 = new Machine(); //can create info object off of Machine class since Machine implements Info
+		info1.showInfo();
+		
+		Info info2 = person1;//info2 points at person1. person1 points at Person. Person implements Info.
+		info2.showInfo();
+		
+		//adding a break to separate outputs more clearly
+		System.out.println();
+		
+		//created method below can use anything point towards the class that implements Info
+		outputInfo(mach1);  //calls showInfo() in Machine
+		outputInfo(person1);//calls showInfo() in Person
+	}
+	
+	private static void outputInfo(Info info) { //static because there is no new App object
+		
+		info.showInfo(); //only method defined in Info interface. more methods are possible.
 		
 	}
 
---
+++ src/IStartable.java
@@ -0,0 +1,8 @@
+
+public interface IStartable {
+	
+	//example of typical interface uses for implementation for methods that would be used repeatedly
+	public void start();
+	public void stop();
+
+}
